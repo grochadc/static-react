e0c91af6d54e370289dfd5bbd566927178d7eab0
Gonzalo Rocha <grochadc@gmail.com>
2019-02-21 19:58:30 -0600

    Setup src and build dirs

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-import ReactDOMServer from "react-dom/server";
-import App from "./App.js";
+import "@babel/polyfill";
+import fs from "fs";
+import util from "util";
 import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "./src/App.js";
 
-console.log(ReactDOMServer.renderToStaticMarkup(<App />));
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const copyFile = util.promisify(fs.copyFile);
+
+String.prototype.interpolate = function(params) {
+  const names = Object.keys(params);
+  const vals = Object.values(params);
+  return new Function(...names, `return \`${this}\`;`)(...vals);
+};
+
+const html = ReactDOMServer.renderToStaticMarkup(<App />);
+
+(async () => {
+  const template = await readFile("./src/template.html", "utf8");
+  const result = template.interpolate({
+    html,
+    title: "Simple e-mail HTML",
+    cssFile: "styles.css"
+  });
+  copyFile("./src/styles.css", "./build/styles.css");
+  await writeFile("./build/index.html", result);
+  console.log("HTML Compiled! Check build/ folder!");
+})().catch(console.error);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
 class App extends Component {
   render() {
-    return <div />;
+    return <div>My first static email</div>;
   }
 }
 
